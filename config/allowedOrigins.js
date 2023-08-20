e4297ec7a6b4f44373fb9f9bb1991d9de7a82280
devon <devonyeung0216@gmail.com>
2023-08-20 14:17:15 +0200

    changed allowed origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://www.whiteboardDJ.com',
-    'https://whiteboardDJ.com'
+    // 'https://www.whiteboardDJ.com',
+    // 'https://whiteboardDJ.com'
+    'https://whiteboarddj.onrender.com'
 ]
 
 module.exports = allowedOrigins
